e038c29dc083c681c2d79ca79040dceecb4bb5e6
Ángel Salvatore <angelhdzsalvatore@gmail.com>
2019-01-17 20:12:48 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+  var http = require('http'),
+  fs = require('fs');
+
+http.createServer(function(req, res){
+  if(req.url.indexOf("favicon.ico")>0){
+    return;
+  }
+  
+  console.log("\=========\n\n");
+  console.log(req);
+  console.log("\=========\n\n");
+  
+  fs.readFile("./index.html", function(err, html) {
+    var html_string = html.toString();
+    var arreglo_parametros = [], parametros ={};
+    var variables = html_string.match(/[^\{\}]+(?=\})/g);
+    var nombre = "";
+    if(req.url.indexOf("?") > 0){
+      var url_data = req.url.split("?");
+      var arreglo_parametros = url_data[1].split("&");
+    }
+    
+    for (var i = arreglo_parametros.length - 1; i >=0; i--){
+      var parametro = arreglo_parametros[i];
+      var param_data = parametro.split("=");
+      parametros[param_data[0]] = [param_data[1]];
+    };
+    
+    for(var i = variables.length - 1; i >= 0; i--){
+      var variable = variables[i];
+      
+      html_string = html_string.replace("{"+variables[i]+"}", parametros[variables[i]]);
+    }
+      
+    res.writeHead(200, {"Content-Type":"text/html" })
+    res.write(html_string);
+    res.end();
+  })
+}).listen(9080);
